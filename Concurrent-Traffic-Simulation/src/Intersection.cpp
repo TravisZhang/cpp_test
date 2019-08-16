20430c3f5b70446fa5496776fff6e2a1f5fa887d
zhangcy2 <zhangcy2@xiaopeng.com>
2019-08-16 22:23:29 +0800

    Changed lck.unlock() position in addVehicleToQueue() to avoid lock while waiting

--- Concurrent-Traffic-Simulation/src/Intersection.cpp
+++ Concurrent-Traffic-Simulation/src/Intersection.cpp
@@ -87,9 +87,9 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
+    lck.unlock();
     while(_trafficLight.getCurrentPhase() == TrafficLight::TrafficLightPhase::red)
         _trafficLight.waitForGreen();
-    lck.unlock();
 }
 
 void Intersection::vehicleHasLeft(std::shared_ptr<Vehicle> vehicle)
